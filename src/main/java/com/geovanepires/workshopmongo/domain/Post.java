e7cf344ea526f9f4cd86c70fc7d2ee08c1e504a4
Geovane Pires <geovane.hau.franca.pires@gmail.com>
2024-02-26 16:17:38 -0300

    Projecao de dados do autor com DTO

--- src/main/java/com/geovanepires/workshopmongo/domain/Post.java
+++ src/main/java/com/geovanepires/workshopmongo/domain/Post.java
@@ -7,6 +7,8 @@ import java.util.Objects;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.geovanepires.workshopmongo.dto.AuthorDTO;
+
 @Document
 public class Post implements Serializable{
 
@@ -18,12 +20,15 @@ public class Post implements Serializable{
 	private Date date;
 	private String title;
 	private String body;
-	private User author;
+	private AuthorDTO author;
+	
+	
+	
 	public Post() {
 		
 	}
 
-	public Post(String id, Date date, String title, String body, User author) {
+	public Post(String id, Date date, String title, String body, AuthorDTO author) {
 		this.id = id;
 		this.date = date;
 		this.title = title;
@@ -62,6 +67,14 @@ public class Post implements Serializable{
 	public void setBody(String body) {
 		this.body = body;
 	}
+	
+	public AuthorDTO getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(AuthorDTO author) {
+		this.author = author;
+	}
 
 	@Override
 	public int hashCode() {
@@ -80,13 +93,7 @@ public class Post implements Serializable{
 		return Objects.equals(id, other.id);
 	}
 
-	public User getAuthor() {
-		return author;
-	}
-
-	public void setAuthor(User author) {
-		this.author = author;
-	}
+	
 	
 	
 	

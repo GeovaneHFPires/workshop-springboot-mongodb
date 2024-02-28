2cbdd1e345a0936dee60c0c0e29001b4561a3bfa
Geovane Pires <geovane.hau.franca.pires@gmail.com>
2024-02-28 15:23:09 -0300

    Acrescentando Comentarios aos Posts

--- src/main/java/com/geovanepires/workshopmongo/config/Instantiation.java
+++ src/main/java/com/geovanepires/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.geovanepires.workshopmongo.domain.Post;
 import com.geovanepires.workshopmongo.domain.User;
 import com.geovanepires.workshopmongo.dto.AuthorDTO;
+import com.geovanepires.workshopmongo.dto.CommentDTO;
 import com.geovanepires.workshopmongo.repository.PostRepository;
 import com.geovanepires.workshopmongo.repository.UserRepository;
 
@@ -40,6 +41,13 @@ public class Instantiation implements CommandLineRunner {
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou para Sao Paulo, Abracos!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		CommentDTO c1 = new CommentDTO("Boa viagem Mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um Otimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		
--- src/main/java/com/geovanepires/workshopmongo/domain/Post.java
+++ src/main/java/com/geovanepires/workshopmongo/domain/Post.java
@@ -1,31 +1,35 @@
 package com.geovanepires.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.geovanepires.workshopmongo.dto.AuthorDTO;
+import com.geovanepires.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable{
 
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	private String id;
 	private Date date;
 	private String title;
 	private String body;
 	private AuthorDTO author;
-	
-	
-	
+	private List<CommentDTO> comments = new ArrayList<>();
+
+
+
 	public Post() {
-		
+
 	}
 
 	public Post(String id, Date date, String title, String body, AuthorDTO author) {
@@ -67,7 +71,7 @@ public class Post implements Serializable{
 	public void setBody(String body) {
 		this.body = body;
 	}
-	
+
 	public AuthorDTO getAuthor() {
 		return author;
 	}
@@ -75,6 +79,14 @@ public class Post implements Serializable{
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+
 
 	@Override
 	public int hashCode() {
@@ -93,9 +105,11 @@ public class Post implements Serializable{
 		return Objects.equals(id, other.id);
 	}
 
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
 }
